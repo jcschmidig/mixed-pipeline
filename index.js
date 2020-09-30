54a8af5597f5f1e45837cc25a9e64d45546de910
jcschmidig <jean-claude@schmidig.email>
2020-09-30 11:44:00 +0200

    minor improvement 3

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 "use strict"
 
 const { error:cError, debug:cDebug} = console
-
-module.exports = function(errorHandler=cError, trace=false) {
+module.exports = function(errHandler=cError, tracingActive=false) {
     const pipeline = [ Promise.resolve([]) ],
 
     actionPipe = (action, res, input) =>
-        Promise.resolve(action(...res.concat(input))),
+        Promise.resolve(action(...res, input)),
     run = ({ func:pipes, res, input }) =>
         Promise.all(pipes.map( pipe => actionPipe(pipe, res, input) )),
-    store = ({ func, res, input, state }) =>
-        (state.set(func.name, actionPipe(func, res, input)), res),
-    restore = async ({ func:name, res, state }) =>
-        res.concat(await state.get(name)),
-    split = ({ func:pipe, res, state }) =>
-        res.concat(res[0].map( arg => pipe.execute(arg, state))),
-    tracing = args => cDebug('>>> trace <<<\n', args),
-    ok = res => res && res.every(v => v !== null),
+    store = ({ func:{name}, func, res, input, state }) =>
+        (state.set(name, actionPipe(func, res, input)), res),
+    restore = ({ func:name, res, state }) =>
+        state.get(name).then(action => [...res, action]),
+    split = ({ func:pipe, res:[args], state }) =>
+        args.map( arg => pipe.execute(arg, state)),
+    trace = args => tracingActive && cDebug('>>> trace <<<\n', args),
+    ok = res => res && res.every(val => val !== null),
 
     execute = (input, state=new Map()) =>
         pipeline.reduce( (promise, { method, func }) => promise
             .then( res => ok(res) && (
-                trace && tracing({ method, func, input:res.concat(input) }),
+                trace({ method, func, input:[...res, input] }),
                 method({ func, res, input, state }) ))
-            .catch( error => { errorHandler({ method, func, input, error }) }) )
+            .catch( err => { errHandler({ method, func, input, err }) }) )
     //
     const ppl = (method, func) => pipeline.push({ method, func })
     return {
