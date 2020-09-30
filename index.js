abb3ae2ea4690afe74d5450808472de69e5fb294
jcschmidig <jean-claude@schmidig.email>
2020-09-30 06:50:12 +0200

    improve error handling

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const { error:cError, debug:cDebug} = console
 
 module.exports = function(errorHandler=cError, trace=false) {
-    const pipeline = [],
+    const pipeline = [ Promise.resolve([]) ],
 
     actionPipe = (action, res, input) =>
         Promise.resolve(action(...res.concat(input))),
     run = ({ func:pipes, res, input }) =>
         Promise.all(pipes.map( pipe => actionPipe(pipe, res, input) )),
-    store = ({ func, res, state, input }) =>
+    store = ({ func, res, input, state }) =>
         (state.set(func.name, actionPipe(func, res, input)), res),
     restore = async ({ func:name, res, state }) =>
         res.concat(await state.get(name)),
@@ -20,11 +20,10 @@ module.exports = function(errorHandler=cError, trace=false) {
 
     execute = (input, state=new Map()) =>
         pipeline.reduce( (promise, { method, func }) => promise
-            .then( res => (
-                trace && tracing({ method, func, input:res, plus:input }),
-                ok(res) && method({ func, res, state, input }) ))
-            .catch( error => errorHandler({ method, func, input, error }) )
-            , Promise.resolve([]) )
+            .then( res => ok(res) && (
+                trace && tracing({ method, func, input:res.concat(input) }),
+                method({ func, res, input, state }) ))
+            .catch( error => { errorHandler({ method, func, input, error }) }) )
     //
     const ppl = (method, func) => pipeline.push({ method, func })
     return {
