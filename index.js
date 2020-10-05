e1dda33167011c29d3c404ec9e04845016c31564
jcschmidig <jean-claude@schmidig.email>
2020-10-05 06:51:04 +0200

    code improvements 3

--- index.js
+++ index.js
@@ -3,49 +3,42 @@
 module.exports = pipeline
 
 const { error:cError, debug:cDebug } = console,
-doAction = (action, res, input) =>
-    Promise.resolve(action(...res, input)),
+doAction = (action, res, input) => Promise.resolve(action(...res, input)),
 
 run = ({ arg:pipes, res, input }) =>
     Promise.all(pipes.map( pipe => doAction(pipe, res, input) )),
 runEx = args => ( void run(args), args.res ),
 
-store = ({ arg:funcs, res, input, state }) => (
-    void funcs.map( func => state.set(func.name, doAction(func, res, input)) ),
-    res ),
+store = ({ arg:funcs, res, input, state }) => ( void funcs.map( func =>
+    state.set(func.name, doAction(func, res, input)) ), res ),
+restore = ({ arg:funcs, res, state }) => Promise.all(funcs.map( ({name}) =>
+    state.get(name) )).then( actions => [ ...res, ...actions ] ),
 
-restore = ({ arg:funcs, res, state }) =>
-    Promise.all(funcs.map( ({name}) => state.get(name) ))
-           .then( actions => [ ...res, ...actions ] ),
-
-split = ({ arg:pipe, res:[args], state }) =>
-    args.map( arg => pipe.execute(arg, state) ),
+split = ({ arg:pipeline, res:[args], state }) =>
+    args.map( arg => pipeline.execute(arg, state) ),
 
 doTrace = (trace, args) => trace && cDebug('>>> trace <<<\n', args),
 pplIsOk = res => Array.isArray(res) && !res.includes(null),
 
 doProcess = (trace, { method, arg }, input, res, state) =>
-    pplIsOk(res) && (
-        void doTrace(trace, { method, arg, input:[...res, input] }),
+    pplIsOk(res) && ( void
+        doTrace(trace, { method, arg, input:[...res, input] }),
         method({ arg, res, input, state }) )
 
 function pipeline(errHandler=cError, trace=false) {
-    const pipeline = new Array(),
-    pipeStart = Promise.resolve(new Array()),
-
+    const pipeline = new Array(Promise.resolve(new Array())),
     execute = (input, state=new Map()) => void pipeline.reduce(
         (pipe, process) => pipe
             .then( res => doProcess(trace, process, input, res, state) )
-            .catch( err => void errHandler({ ...process, input, err }) )
-        , pipeStart )
+            .catch( err => void errHandler({ ...process, input, err }) )),
     //
-    const ppl = (method, arg) => pipeline.push({ method, arg })
+    ppl = (method, arg) => pipeline.push({ method, arg })
     return {
         run: function(...pipes) { ppl(run, pipes); return this },
         runShadow: function(...pipes) { ppl(runEx, pipes); return this },
-        store: function(...pipes) { ppl(store, pipes); return this },
-        restore: function(...pipes) { ppl(restore, pipes); return this },
-        split: function(pipe) { ppl(split, pipe); return this },
+        store: function(...funcs) { ppl(store, funcs); return this },
+        restore: function(...funcs) { ppl(restore, funcs); return this },
+        split: function(pipeline) { ppl(split, pipeline); return this },
         execute
     }
 }
