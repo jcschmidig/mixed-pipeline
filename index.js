357a2846f19d6a54cfb220c9f1e6aa458c9ca0c4
jcschmidig <jean-claude@schmidig.email>
2020-10-05 08:30:45 +0200

    code improvements 4

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ doAction = (action, res, input) => Promise.resolve(action(...res, input)),
 
 run = ({ arg:pipes, res, input }) =>
     Promise.all(pipes.map( pipe => doAction(pipe, res, input) )),
-runEx = args => ( void run(args), args.res ),
+runShadow = args => ( void run(args), args.res ),
 
 store = ({ arg:funcs, res, input, state }) => ( void funcs.map( func =>
     state.set(func.name, doAction(func, res, input)) ), res ),
 restore = ({ arg:funcs, res, state }) => Promise.all(funcs.map( ({name}) =>
     state.get(name) )).then( actions => [ ...res, ...actions ] ),
 
-split = ({ arg:pipeline, res:[args], state }) =>
+split = ({ arg:[pipeline], res:[args], state }) =>
     args.map( arg => pipeline.execute(arg, state) ),
 
 doTrace = (trace, args) => trace && cDebug('>>> trace <<<\n', args),
@@ -30,15 +30,9 @@ function pipeline(errHandler=cError, trace=false) {
     execute = (input, state=new Map()) => void pipeline.reduce(
         (pipe, process) => pipe
             .then( res => doProcess(trace, process, input, res, state) )
-            .catch( err => void errHandler({ ...process, input, err }) )),
+            .catch( err => void errHandler({ ...process, input, err }) ))
     //
-    ppl = (method, arg) => pipeline.push({ method, arg })
-    return {
-        run: function(...pipes) { ppl(run, pipes); return this },
-        runShadow: function(...pipes) { ppl(runEx, pipes); return this },
-        store: function(...funcs) { ppl(store, funcs); return this },
-        restore: function(...funcs) { ppl(restore, funcs); return this },
-        split: function(pipeline) { ppl(split, pipeline); return this },
-        execute
-    }
+    return [ run, runShadow, store, restore, split ].reduce( (out, method) =>
+        Object.defineProperty(out, method.name, { value: function(...arg)
+            { pipeline.push({ method, arg }); return this } }), { execute } )
 }
