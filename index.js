9a14fd14cf713fc098016169942f63be25c100e4
jcschmidig <jean-claude@schmidig.email>
2020-10-10 14:51:54 +0200

    reorganize code

--- index.js
+++ index.js
@@ -2,31 +2,6 @@
 
 /*
     Usage example (see https://github.com/jcschmidig/mixed-pipeline#readme)
-    -----------------------------------------------------------------------
-
-    import fs from 'fs/promises'
-    import pipeline from 'mixed-pipeline'
-
-    pipeline()
-        .store(getTemplate)
-        .run(findPaths)     // returns an array of subPaths
-        .split(pipeline()   // executes a new pipeline for every subPath
-            .restore(getTemplate)
-            .run(writeConfig)
-        )
-    .execute("/myPath")     // starts the pipeline with the given path
-
-    function getTemplate(path) {
-        return fs.readFile(path+'/template.json')
-    }
-
-    function findPaths(path) {
-        return fs.readdir(path)
-    }
-
-    function writeConfig(template, subPath) {
-        void fs.writeFile(subPath+'/config.json', template)
-    }
  */
 
 module.exports = function( errHandler = console.error ) {
@@ -44,14 +19,14 @@ module.exports = function( errHandler = console.error ) {
         $state = new Map()  // holds the stored functions for all pipelines
         //
     ) => void process(
-            pipeline,
-            (pipe, item) => pipe
-                // waits for the promise to be resolved and gives the result
-                // to the next process item
-                .then( res => doProcess(item, $input, res, $state) )
-                // catches any error occurring during the pipeline's processing
-                .catch( err => void errHandler({ ...item, $input, err }) )
-        ),
+        pipeline,
+        (pipe, item) => pipe
+            // waits for the promise to be resolved and gives the result
+            // to the next process item
+            .then( res => processItem(item, $input, res, $state) )
+            // catches any error occurring during the pipeline's processing
+            .catch( err => void errHandler({ ...item, input:$input, err }) )
+    ),
 
     // adds method and arguments to the pipeline,
     createMethod = method => function(...arg) { add(method, arg); return this },
@@ -68,20 +43,13 @@ const
 process = (obj, ...args) => Array.prototype.reduce.apply(obj, args),
 
 // processes the current item of the pipeline
-doProcess = ({ method, arg }, input, res, state) =>
+processItem = ({ method, arg }, input, res, state) =>
     // check and execute the method (one of the methods below)
-    pplIsOk(res) && method({ arg, res, input, state }),
+    pipelineIsOk(res) && method({ arg, res, input, state }),
 
 // checks if the result of the current pipe is ok
-pplIsOk = (res) =>
+pipelineIsOk = (res) =>
     Array.isArray(res) && !res.includes(null),
-
-// ensures that every action returns as a promise
-doAction = (action, res, input) =>
-    Promise.resolve(action(...res, input)),
-
-// allows a method to be called from within another method
-method = (name, args) => methods.find(method => method.name === name)(args),
 
 // this array has two purposes (see exported function)
 //  - the method definition is used to build the pipeline's interface
@@ -90,30 +58,30 @@ method = (name, args) => methods.find(method => method.name === name)(args),
 methods = [
     // runs all functions in the pipe concurrently
     function run({ arg:funcs, res, input }) {
-        return Promise.all(funcs.map( func => doAction(func, res, input) ))
+        return Promise.all( funcs.map( runFunction(res, input) ))
     },
     // runs the pipe ignoring the result
     function runShadow(args) {
         // calls the already defined function 'run'
-        method('run', args)
+        callMethod('run', args)
         return args.res
     },
 
     // stores the result of the function(s) in the state Map
     function store({ arg:funcs, res, input, state }) {
-        funcs.map( func => state.set(func.name, doAction(func, res, input)) )
+        funcs.map( pack(state, res, input) )
         return res
     },
-    // inserts the stored function(s) of the state Map into the pipe
+    // restores the requested results of the state Map into the pipe
     function restore({ arg:funcs, res, state }) {
-        return Promise.all(funcs.map( func => state.get(func.name) ))
-                      .then( actions => [ ...res, ...actions ] )
+        return Promise.all( funcs.map( unpack(state) ))
+                      .then( states => [ ...res, ...states ] )
     },
 
     // takes the array of a resulting function and executes
     // the defined pipeline for every item
     function split({ arg:[pipeline], res:[args], state }) {
-        return args.map( arg => pipeline.execute(arg, state) )
+        return args.map( execPipeline(pipeline, state) )
     },
 
     // traces the input parameters being consumed by the next method
@@ -121,4 +89,20 @@ methods = [
         console.debug(`${comment}\n`, { input: [ ...res, input ] }, '\n')
         return res
     }
-]
+],
+
+// allows a method to be called from within another method
+callMethod = (name, args) => methods.find(method => method.name === name)(args),
+
+// ensures that every func returns as a promise
+createPromise = (func, res, input) => Promise.resolve(func(...res, input)),
+
+// Helpers
+runFunction = (res, input) => func => createPromise(func, res, input),
+
+pack = (state, res, input) =>
+    func => state.set(func.name, createPromise(func, res, input)),
+
+unpack = state => func => state.get(func.name),
+
+execPipeline = (pipeline, state) => input => pipeline.execute(input, state)
