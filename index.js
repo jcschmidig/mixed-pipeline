044e805cd00ccb648bd35ccb669e7bbea170bdc3
jcschmidig <jean-claude@schmidig.email>
2020-10-22 08:55:37 +0200

    consolidate

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict"
-
 /*
     Usage: see https://github.com/jcschmidig/mixed-pipeline#readme
  */
@@ -37,75 +36,57 @@ const
 // this array has two purposes (see exported function)
 //  - the method definition is used to build the pipeline's interface
 //  - the method body is being executed while processing the pipeline
-// !! every method defined here is automatically populated to the interface !!
 METHODS = [
     // runs all functions in the pipe concurrently
-    function run({ arg:funcs, res, input }) {
-        return funcs.concurrent(runFunction, res, input)
-    },
+    function run({ arg:funcs, res, input })
+        { return funcs.concurrent( fRun(res, input) ) },
+
     // runs the pipe ignoring the result
-    function runShadow(args) {
-        METHODS.exec('run', args)
-        return args.res
-    },
+    function runShadow(args) { return void METHODS.exec('run', args) },
 
     // stores the result of the function(s) in the state Map
-    function store({ arg:funcs, res, input, state }) {
-        funcs.concurrent(pack, state, res, input)
-        return res
-    },
-    // restores the requested results of the state Map into the pipe
-    async function restore({ arg:funcs, res, state }) {
-        return [ ...res, ...(await funcs.concurrent(unpack, state)) ]
-    },
+    function store({ arg:funcs, res, input, state })
+        { return void funcs.concurrent( fStore(state, res, input) ) },
 
-    // takes the array of a resulting function and executes
-    // the defined pipeline for every item concurrently
-    function split({ arg:[pipeline], res:[args], state }) {
-        return args.concurrent(execPipeline, pipeline, state)
-    },
+    // restores the requested results of the state Map into the pipe
+    async function restore({ arg:funcs, res, state })
+        { return [ ...res, ...(await funcs.concurrent( fRestore(state) )) ] },
+
+    // takes an array and executes the new pipeline for every item concurrently
+    function split({ arg:[pipeline], res:[args], state })
+        { return args.concurrent( pExecute(pipeline, state) ) },
 
     // traces the input parameters being consumed by the next method
-    function trace({ arg:[comment='>>> trace', output=debug], res, input }) {
-        output.exec(comment, { input: [ ...res, input ] })
-        return res
-    }
+    function trace({ arg:[comment='>>> trace', output=oDebug], res, input })
+        { return void output.exec(comment, { input: [ ...res, input ] }) }
 ],
 
-// processes the current item of the pipeline
 processItem = ({ method, arg }, input, res, state) =>
     // check and execute the method (one of the METHODS above)
-    pipelineIsOk(res) && method.exec({ arg, res, input, state }),
-//
-pipelineIsOk = res =>
-    // no error catched & not stopped by consumer
-    Array.isArray(res) && !res.includes(null),
+    pipelineIsOk(res) && (method.exec({ arg, res, input, state }) || res),
 
-// Helpers
-runFunction = (res, input) => func => func.exec(...res, input),
+// checking result:   no error catched  and not stopped by consumer
+pipelineIsOk = res => Array.isArray(res) && !res.includes(null),
 
-pack = (state, res, input) => func =>
-    state.set(func.name, func.exec(...res, input)),
-unpack = state => func => state.get(func.name),
+// helper
+fMerge = (f, res, input) => f.exec(...res, input),
+fRun = (res, input) => f => fMerge(f, res, input),
+fStore = (state, res, input) => f => state.set(f.name, fMerge(f, res, input)),
+fRestore = state => f => state.get(f.name),
+pExecute = (pipeline, state) => input => pipeline.execute(input, state),
+oDebug = (comment, arg) => console.debug(`${comment}\n`, arg, '\n')
 
-execPipeline = (pipeline, state) => input => pipeline.execute(input, state),
+Function.prototype.exec = function(...args) { return this.apply(null, args) }
 
-debug = (comment, arg) => console.debug(`${comment}\n`, arg, '\n')
+Array.prototype.exec = function(name, ...args)
+    { this.find(func => func.name === name).exec(...args) }
 
-Function.prototype.exec = function(...args) {
-    return this.apply(null, args)
-}
-Array.prototype.exec = function(name, ...args) {
-    this.find(func => func.name === name).exec(...args)
-}
-Array.prototype.toObject = function(func) {
-    return Object.fromEntries( this.map( value =>
-        [ value.name, func ? func.exec(value) : value ]
-    ))
-}
-Array.prototype.process = function(reducer, initValue=[]) {
-    return this.reduce( reducer, initValue )
-}
-Array.prototype.concurrent = function(action, ...args) {
-    return Promise.all( this.map( action.exec(...args) ))
-}
+Array.prototype.toObject = function(func)
+    { return Object.fromEntries( this.map( value =>
+        [ value.name, func ? func.exec(value) : value ] )) }
+
+Array.prototype.process = function(reducer, initValue=[])
+    { return this.reduce( reducer, initValue ) }
+
+Array.prototype.concurrent = function(action)
+    { return Promise.all( this.map( action )) }
