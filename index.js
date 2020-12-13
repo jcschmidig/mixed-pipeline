1e20db1e1e14b8cb9d98fae92e479e62b043c170
jcschmidig <jean-claude@schmidig.email>
2020-12-13 16:38:53 +0100

    use object for methods

--- index.js
+++ index.js
@@ -4,17 +4,15 @@
  */
 
 module.exports = function( $errHandler = console.error ) {
-    const
-    $pipeline = new List(),
-
-    addToPipeline = method =>
-        function(...funcs) {
-            $pipeline.push({ method, funcs: List.from(funcs) })
-            return this
-        },
-
+    const $pipeline = new Array(),
+    //
+    addToPipeline = method => function(...funcs) {
+        $pipeline.push({ method, funcs })
+        return this
+    },
+    //
     execute = (input, state=new Map()) =>
-        $pipeline.process(
+        $pipeline.reduce(
             async (pipe, item) => {
                 let res
                 // every item propagates the resulting pipe to the next item
@@ -22,59 +20,44 @@ module.exports = function( $errHandler = console.error ) {
                 catch(err) { $errHandler({ ...item, input, err }) }
                 //
                 return res
-            }
-        )
+            }, [] )
 
-    // adds all METHODS and the execute function to the interface
-    return { execute, ...METHODS.build( wrapper(addToPipeline) ) }
+    // register all METHODS and the execute function to the interface
+    return { execute, ...register(METHODS, addToPipeline) }
 }
 
-const List = class extends Array {
-    build(processor) { return this.reduce( processor, new Object ) }
-    process(processor) { return void this.reduce( processor, new Array() ) }
-    concurrent(processor) { return Promise.all( this.map( processor )) }
-    findByName(value) { return this.find( ({ name }) => name === value ) }
+const METHODS = {
+    //
+    run({ funcs, args }) { return concurrent(funcs, fapply(args)) },
+    runShadow(props)     { this.run(props) },
+    //
+    store({ funcs, args, state })
+        { concurrent(funcs, fset(state, fapply(args))) },
+    //
+    async restore({ funcs, pipe, state })
+        { return pipe.concat( await concurrent(funcs, fget(state))) },
+    //
+    split({ funcs:pipelines, pipe:[input], state })
+        { concurrent(pipelines, pexec(input, state)) },
+    //
+    trace({ funcs:[comment='>>> trace', output=debug], args })
+        { output(comment, { args }) }
 },
-// this array has two purposes (see exported function)
-//  - the method definition is used to build the pipeline's interface
-//  - the method body is being executed while processing the pipeline
-METHODS = List.from([
-    // runs all functions in the pipe concurrently
-    function run({ funcs, args }) { return funcs.concurrent( fapply(args) ) },
-
-    // runs the pipe ignoring the result
-    function runShadow(props) { return void runMethod('run', props) },
-
-    // stores the result of the function(s) in the state Map
-    function store({ funcs, args, state })
-        { return void funcs.concurrent( fset(state, args) ) },
-
-    // restores the requested results of the state Map into the pipe
-    async function restore({ funcs, pipe, state })
-        { return pipe.concat( await funcs.concurrent( fget(state) )) },
-
-    // takes an array and executes the new pipelines for every item concurrently
-    function split({ funcs:pipelines, pipe:[input], state })
-        { return void pipelines.concurrent( pexec(input, state) ) },
-
-    // traces the input parameters being consumed by the next method
-    function trace({ funcs:[comment='>>> trace', output=debug], args })
-        { return void output(comment, { args }) }
-]),
 
 processItem = ({ method, funcs }, input, pipe, state) =>
     isBroken(pipe)
-        || method({ funcs, pipe, args:pipe.concat(input), state })
+        || METHODS[method] ({ funcs, pipe, args:pipe.concat(input), state })
         || pipe,
 
 // checking pipe:         error catched or stopped by consumer
 isBroken = pipe => !Array.isArray(pipe) || pipe.includes(null),
 
 // helper
-wrapper = id => (obj, fn) => ( obj[fn.name] = id(fn), obj ),
-runMethod = (name, arg) => METHODS.findByName(name).call(this, arg),
+register = (obj, gen) => Object.keys(obj).reduce( (o, key) =>
+    Object.defineProperty(o, key, { value: gen(key), enumerable: true }), {} ),
+concurrent = (obj, processor) => Promise.all( obj.map( processor )),
 fapply =       args  => func => func.apply(this, args),
-fset = (state, args) => func => state.set(func.name, fapply(args)(func)),
+fset = (state, farg) => func => state.set(func.name, farg(func)),
 fget =  state        => func => state.get(func.name),
 pexec = (args, state) => ppl => args.map( input => ppl.execute(input, state) ),
 debug = (comment, arg) => console.debug(`${comment}\n`, arg, '\n')
