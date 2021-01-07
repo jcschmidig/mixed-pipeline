98971334c8ab93965c10037c9e7fcdfb11f9f483
jcschmidig <jean-claude@schmidig.email>
2021-01-07 07:53:17 +0100

    replace Map with Object

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = function(disp=console.error) {
     const $ppl = new Array(),
     register = name => function(...fn) { $ppl.push([ name, fn ]); return this },
-    execute = (input, state=new Map()) => $ppl.reduce( ($pipe, item) => $pipe
+    execute = (input, state={}) => $ppl.reduce( ($pipe, item) => $pipe
         .then( pipe => Array.isArray(pipe) && process(item, input, pipe, state))
         .catch( err => void disp({ item, input, err }) ), Promise.resolve([]) )
     //
@@ -13,11 +13,11 @@ module.exports = function(disp=console.error) {
 //
 const METHODS = {
     run     ({ funcs, args })        { return pack(funcs, apply(args)) },
-    restore ({ funcs, pipe, state }) { return pack(funcs, map(state), pipe) },
-    store   ({ funcs, args, state }) { funcs.map( map(state, apply(args)) ) },
+    restore ({ funcs, pipe, state }) { return pack(funcs, prop(state), pipe) },
+    store   ({ funcs, args, state }) { funcs.map( prop(state, apply(args)) ) },
     split   ({ funcs, pipe, state }) { funcs.map( exec(state, pipe[0]) ) },
     trace   ({ funcs:[cmt], args })  { console.debug(`${cmt}\n`, args, '\n') },
-    runShadow (props)                { this.run(props) }
+    runShadow (options)              { this.run(options) }
 },
 //
 process = ([name, funcs], input, pipe, state) => pipe.includes(null) ||
@@ -26,5 +26,5 @@ process = ([name, funcs], input, pipe, state) => pipe.includes(null) ||
 transform = (obj, proc)     => Object.fromEntries(Object.keys(obj).map( proc )),
 pack = (lst, proc, coll=[]) => Promise.all(coll.concat(lst.map( proc ))),
 apply = arg          => fnc => fnc.apply(this, arg),
-map   = (m, arg)     => fnc => arg ? m.set(fnc.name, arg(fnc)) : m.get(fnc.name),
+prop  = (obj, arg)   => fnc => arg ? obj[fnc.name] = arg(fnc) : obj[fnc.name],
 exec  = (state, arg) => ppl => arg.map( input => ppl.execute(input, state) )
